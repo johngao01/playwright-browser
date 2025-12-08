2297fff35f0616c0159c165963e4d5cc85d030be
johngao01 <1649868243@qq.com>
2025-12-08 16:59:11 +0800

    main脚本

---
+++ main.py
@@ -0,0 +1,44 @@
+# main.py
+import asyncio
+
+from playwright.async_api import ProxySettings, async_playwright
+import login_weibo
+import login_instagram
+
+
+async def main():
+    # 替换你的代理端口
+    PROXY_SERVER = "http://127.0.0.1:10808"
+    proxy = ProxySettings(server=PROXY_SERVER)
+    USER_DATA_DIR = './browser_data'
+    async with async_playwright() as p:
+        context = await p.chromium.launch_persistent_context(
+            user_data_dir=USER_DATA_DIR,
+            headless=False,
+            channel="msedge",
+            # 这步操作直接去掉了“msedge 正受到自动测试软件的控制”的横幅
+            # ignore_default_args=["--enable-automation"],
+            proxy=proxy,
+            args=[
+                "--autoplay-policy=no-user-gesture-required",
+                "--mute-audio",
+                "--start-maximized"  # 启动时最大化
+            ],
+            no_viewport=True,  # 必须开启，否则 maximize 不生效，页面会受限于默认窗口大小
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        )
+        weibo_page = context.pages[0]
+        await login_weibo.login(context, weibo_page)
+        instagram_page = await context.new_page()
+        await login_instagram.login(context, instagram_page)
+        await login_instagram.save_cookies(context)
+        await login_weibo.save_cookies(context)
+
+        await context.wait_for_event("close", timeout=0)
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("程序已停止")
